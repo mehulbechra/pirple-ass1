64e2c095ab2ca691e8608327ed32da627f0a3182
Mehul Bechra <bechra.mehul@gmail.com>
2018-11-18 19:04:22 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,87 @@
+/*
+*   Main file for the API
+*/
+
+// Dependencies
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+const fs = require('fs');
+const config = require('./config');
+
+// Instantiate & start http server
+const httpServer = http.createServer((req, res) => unifiedServer(req,res));
+httpServer.listen(config.httpPort, () => console.log("The serve is listening on port "+config.httpPort));
+
+// Instantiate & start https server
+const httpsServerOptions = {
+    'key' : fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem') 
+};
+const httpsServer = https.createServer(httpsServerOptions ,(req,res) => unifiedServer(req, res));
+httpsServer.listen(config.httpsPort, () => console.log("The serve is listening on port "+config.httpsPort));
+
+// Server logic
+const unifiedServer = (req,res) => {
+
+    // Get the parsedUrl and trim it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    
+    // Get query object
+    const queryObject = parsedUrl.query;
+
+    // Get Headers
+    const headers = req.headers;
+
+    // Get method
+    const method = req.method;
+
+    // Get the payload
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', (data) => buffer += decoder.write(data));
+
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Choose the handler based on router
+        const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        // Data to be passed to handler
+        const data = {
+            'trimmedPath' : trimmedPath,
+            'queryObject' : queryObject,
+            'headers' : headers,
+            'method' : method,
+            'payload' : buffer
+        };
+
+        chosenHandler(data, (statusCode, payload) => {
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+            payload = typeof(payload) == 'object' ? payload : {};
+            var payloadString = JSON.stringify(payload);
+
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log("Returned response: ",statusCode, payloadString);
+        });
+    });
+};
+
+// Handlers
+const handlers = {};
+
+// Hello handler
+handlers.hello = (data, callback) => { callback(200, {'message': 'A message in JSON format'}) };
+
+// NotFound Handler
+handlers.notFound = (data, callback) => { callback(404) };
+
+// Router
+const router = {
+    'hello': handlers.hello
+};
---
+++ readme.md
@@ -0,0 +1,4 @@
+A simple api that routes hello path to return a JSON data.
+
+run node index.js
+Send a post request to localhost:3000/hello
