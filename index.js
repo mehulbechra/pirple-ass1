e5691393df7407acc65fb8f8d21690b9869782a6
Mehul Bechra <bechra.mehul@gmail.com>
2018-12-01 17:36:55 +0530

    Server is now running on multiple cores

--- index.js
+++ index.js
@@ -1,87 +1,35 @@
 /*
-*   Main file for the API
+* Primary file for the API
 */
 
 // Dependencies
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const fs = require('fs');
-const config = require('./config');
+const cluster = require('cluster');
+const os = require('os');
+const server = require('./lib/server');
 
-// Instantiate & start http server
-const httpServer = http.createServer((req, res) => unifiedServer(req,res));
-httpServer.listen(config.httpPort, () => console.log("The serve is listening on port "+config.httpPort));
+// Declare the app
+const app = {};
 
-// Instantiate & start https server
-const httpsServerOptions = {
-    'key' : fs.readFileSync('./https/key.pem'),
-    'cert': fs.readFileSync('./https/cert.pem') 
-};
-const httpsServer = https.createServer(httpsServerOptions ,(req,res) => unifiedServer(req, res));
-httpsServer.listen(config.httpsPort, () => console.log("The serve is listening on port "+config.httpsPort));
+// Init function
+app.init = (callback) => {
+  // If on master thread...
+  if (cluster.isMaster) {
+    // Nothing to start in the master process
 
-// Server logic
-const unifiedServer = (req,res) => {
-
-    // Get the parsedUrl and trim it
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    
-    // Get query object
-    const queryObject = parsedUrl.query;
-
-    // Get Headers
-    const headers = req.headers;
-
-    // Get method
-    const method = req.method;
-
-    // Get the payload
-    const decoder = new StringDecoder('utf-8');
-    let buffer = '';
-    req.on('data', (data) => buffer += decoder.write(data));
-
-    req.on('end', () => {
-        buffer += decoder.end();
-
-        // Choose the handler based on router
-        const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-        // Data to be passed to handler
-        const data = {
-            'trimmedPath' : trimmedPath,
-            'queryObject' : queryObject,
-            'headers' : headers,
-            'method' : method,
-            'payload' : buffer
-        };
-
-        chosenHandler(data, (statusCode, payload) => {
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-            payload = typeof(payload) == 'object' ? payload : {};
-            const payloadString = JSON.stringify(payload);
-
-            res.setHeader('Content-Type', 'application/json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
-            console.log("Returned response: ",statusCode, payloadString);
-        });
-    });
+    // Fork the process
+    for (let i = 0; i < os.cpus().length; i += 1) {
+      cluster.fork();
+    }
+  } else {
+    // Start the server
+    server.init();
+  }
 };
 
-// Handlers
-const handlers = {};
+// Self invoking only if directly required
+if (require.main === module) {
+  app.init(() => {});
+}
 
-// Hello handler
-handlers.hello = (data, callback) => { callback(200, {'message': 'A message in JSON format'}) };
-
-// NotFound Handler
-handlers.notFound = (data, callback) => { callback(404) };
-
-// Router
-const router = {
-    'hello': handlers.hello
-};
+// Export the app
+module.exports = app;
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
-### A simple api that routes hello path to return a JSON data.
+### A simple api that routes hello path to return JSON data.
 
 - run node index.js
 - Send a post request to localhost:3000/hello
+
+The servers are running on a multiple cores using the cluster module.
